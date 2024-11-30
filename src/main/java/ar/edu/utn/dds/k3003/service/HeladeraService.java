e561dcb1d61f4b07fea8478414effb3311b2d5c9
Gabriel <gabriellaterza@outlook.com.ar>
2024-11-29 23:37:18 -0300

    Se pueden deshabilitar heladeras con viandas.

--- src/main/java/ar/edu/utn/dds/k3003/service/HeladeraService.java
+++ src/main/java/ar/edu/utn/dds/k3003/service/HeladeraService.java
@@ -67,10 +67,6 @@ public class HeladeraService {
     public HabilitacionDTO deshabilitar(Integer heladeraId){
         Heladera heladera = findHeladeraById(heladeraId);
 
-        if(heladera.getCantidadDeViandas()>0){
-            throw new IllegalStateException("No se puede deshabilitar la heladera porque posee "+heladera.getCantidadDeViandas()+" viandas.");
-        }
-
         heladera.deshabilitar();
         updateHeladera(heladera);
         return new HabilitacionDTO(heladeraId, HabilitacionEnum.INHABILITADA);
